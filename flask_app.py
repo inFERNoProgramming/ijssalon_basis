4185d90f4f248fe54c85e74ff999f8eb33ff7ca1
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-07 10:07:30 +0200

    prijzen en recepten pagina's toegevoegd

--- flask_app.py
+++ flask_app.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def home():
     return "Welcome op mijn eerste Flask-Website"
 
+@app.route('/prijzen')
+def prijzen():
+    return "Binnenkort verschijnen hier onze actuele prijzen"
+
+@app.route('/recepten')
+def recepten():
+    return "Binnenkort verschijnen hier enkele recepten"
+
 if __name__ == '__main__':
     app.run(port=5000, debug = True)
