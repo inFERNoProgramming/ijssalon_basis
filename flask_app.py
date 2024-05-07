e478f18dccd49c6f24f7bb005d5321efcb44400d
inFERNo <168463102+inFERNoProgramming@users.noreply.github.com>
2024-05-07 10:45:51 +0200

    routes aangemaakt

--- flask_app.py
+++ flask_app.py
@@ -1,17 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome op mijn eerste Flask-Website"
+    return render_template("home.html")
 
 @app.route('/prijzen')
 def prijzen():
-    return "Binnenkort verschijnen hier onze actuele prijzen"
+    items = [
+        {
+            "product": "vanille-ijs 1 liter",
+            "prijs": "2 euro"
+        },
+        {
+            "product": "chocolade-ijs 1 liter",
+            "prijs": "2 euro"
+        }
+    ]
+    return render_template("prijzen.html", items=items)
 
 @app.route('/recepten')
 def recepten():
-    return "Binnenkort verschijnen hier enkele recepten"
+    items = [
+        {
+            "recept": "Tiramiso di nona",
+            "img":"tiramisu.png"
+        },
+        {
+            "recept": "Ijstaart met chocolade",
+            "img": "ijstaart.png"
+        }
+    ]
+    return render_template("recepten.html", items=items)
 
 if __name__ == '__main__':
     app.run(port=5000, debug = True)
